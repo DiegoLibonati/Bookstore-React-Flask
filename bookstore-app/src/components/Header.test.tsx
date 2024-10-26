9b17eac5c4853e6dd7cce339b3a357de0780af07
Die <dlibonati@leafnoise.io>
2024-10-26 14:27:07 -0300

    feat: header test added

---
+++ bookstore-app/src/components/Header.test.tsx
@@ -0,0 +1,25 @@
+import { screen, render } from "@testing-library/react";
+
+import { Header } from "./Header";
+
+import { images } from "../assets/export";
+
+const renderComponent = (): { container: HTMLElement } => {
+  const { container } = render(<Header />);
+
+  return {
+    container: container,
+  };
+};
+
+test("The library logo must be rendered.", () => {
+  renderComponent();
+
+  const altImageLogo = "logo";
+
+  const imgElement = screen.getByAltText(altImageLogo);
+
+  expect(imgElement).toBeInTheDocument();
+  expect(imgElement).toHaveAttribute("src", images.libraryLogo);
+  expect(imgElement).toHaveAttribute("alt", altImageLogo);
+});
--- bookstore-app/src/components/Header.tsx
+++ bookstore-app/src/components/Header.tsx
@@ -1,9 +1,11 @@
+import { images } from "../assets/export";
+
 import "../css/header.css";
 
 export const Header = (): JSX.Element => {
   return (
     <header className="header_container">
-      <img className="header_logo" src="libreria.png" alt="logo"></img>
+      <img className="header_logo" src={images.libraryLogo} alt="logo"></img>
     </header>
   );
 };
